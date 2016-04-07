c65142488cf8f366473841c6d197b824a4e2d4d1
Renze Nicolai <renze@rnplus.nl>
2016-04-07 20:03:05 +0200

    removed debug code

--- hold/hold.js
+++ hold/hold.js
@@ -5,28 +5,17 @@ module.exports = function(RED) {
         var node = this;
         var msg = {};
         node.context().storedmessage = node.context().storedmessage || null;
-
         this.on('input', function (msg) {
             if ((msg.trigger==null)||(msg.trigger!=true)) {
-              node.warn("Stored message: "+msg.payload);
               node.context().storedmessage = msg;
             } else {
-              node.warn("Hold triggered!");
               if(node.context().storedmessage!=null) {
                 node.send(node.context().storedmessage);
               } else {
-                node.warn("No message in buffer.");
+                node.warn("Hold '"+n.name+"' triggered without message in buffer.");
               }
             }
         });
-
-        this.on("close", function() {
-
-        });
     }
-
-    // Register the node by name. This must be called before overriding any of the
-    // Node functions.
     RED.nodes.registerType("hold",holdNode);
-
 }
