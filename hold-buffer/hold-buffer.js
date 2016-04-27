1be4f694ff3b5b580bf4e4723a1c002325c6ae38
Taras Emelyanenko <php.laboratory@gmail.com>
2016-04-27 11:59:47 +0300

    add status text

--- hold-buffer/hold-buffer.js
+++ hold-buffer/hold-buffer.js
@@ -6,16 +6,33 @@ module.exports = function(RED) {
         var msg = {};
         if(! node.context().storedmessage) {
             node.context().storedmessage =[];
+            node.status({fill: "green", shape: "ring", text: "0 items"});
         }
 
         this.on('input', function (msg) {
             if ((msg.trigger==null)||(msg.trigger!=true)) {
               node.context().storedmessage.push(msg);
+                this.node.status({fill: "green", shape: "dot", text: ""+ node.context().storedmessage.length +"items"});
             } else {
               if(node.context().storedmessage!=null && node.context().storedmessage.length>0) {
                 node.send(node.context().storedmessage.pop());
+                  if(node.context().storedmessage.length>0) {
+                      this.node.status({
+                          fill: "green",
+                          shape: "dot",
+                          text: "" + node.context().storedmessage.length + "items"
+                      });
+                  } else {
+                      this.node.status({
+                          fill: "green",
+                          shape: "ring",
+                          text: "" + node.context().storedmessage.length + "items"
+                      });
+
+                  }
               } else {
                 node.warn("Hold '"+n.name+"' triggered without message in buffer.");
+                  this.node.status({fill: "red", shape: "ring", text: ""+ node.context().storedmessage.length +"items"});
               }
             }
         });
