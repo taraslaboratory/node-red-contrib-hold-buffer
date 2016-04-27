c5b41ddc5b908a41953c63fad1ba211386710689
Taras Emelyanenko <php.laboratory@gmail.com>
2016-04-27 11:22:37 +0300

    Add buffer support

--- hold-buffer/hold-buffer.js
+++ hold-buffer/hold-buffer.js
@@ -4,18 +4,21 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         var node = this;
         var msg = {};
-        node.context().storedmessage = node.context().storedmessage || null;
+        if(! node.context().storedmessage) {
+            node.context().storedmessage =[];
+        }
+
         this.on('input', function (msg) {
             if ((msg.trigger==null)||(msg.trigger!=true)) {
-              node.context().storedmessage = msg;
+              node.context().storedmessage.push(msg);
             } else {
-              if(node.context().storedmessage!=null) {
-                node.send(node.context().storedmessage);
+              if(node.context().storedmessage!=null && node.context().storedmessage.length()>0) {
+                node.send(node.context().storedmessage.pop());
               } else {
                 node.warn("Hold '"+n.name+"' triggered without message in buffer.");
               }
             }
         });
     }
-    RED.nodes.registerType("hold",holdNode);
+    RED.nodes.registerType("hold-buffer",holdNode);
 }
